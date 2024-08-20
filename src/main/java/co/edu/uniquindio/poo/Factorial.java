2a9409784f0561b6c4dbd24febad30dd30263ef9
Pedro Jose Soto R. <pedroj.sotor@uqvirtual.edu.co>
2024-08-20 16:32:17 -0500

    .

--- src/main/java/co/edu/uniquindio/poo/Factorial.java
+++ src/main/java/co/edu/uniquindio/poo/Factorial.java
@@ -4,21 +4,23 @@ import java.util.Scanner;
 
 public class Factorial {
 
-    public static int newFactorial(Scanner entrada){
+    public static int newFactorial(Scanner entrada) {
         System.out.println("ingrese el numero del cual quiere saber su factorial: ");
-        int facto=entrada.nextInt();
+        int facto = entrada.nextInt();
         return facto;
 
     }
+
     public static long calcularFactorial(int facto) {
         long resultado = 1;
         for (int i = facto; i > 0; i--) {
-            resultado *= i;//es lo mismo que decir resultado=resultado*i
+            resultado *= i;// es lo mismo que decir resultado=resultado*i
         }
         return resultado;
     }
+
     public static String generarMensaje(int num1, long resultado) {
-        String mensaje = "El resultado de tu factorial del numero " + num1 +  " es de: " + resultado;
+        String mensaje = "El resultado de tu factorial del numero " + num1 + " es de: " + resultado;
         return mensaje;
     }
 
@@ -26,12 +28,11 @@ public class Factorial {
         System.out.println(mensaje);
     }
 
-    
     public static void main(String[] args) {
-        Scanner entrada=new Scanner(System.in);
-        int factorial=newFactorial(entrada);
-        long resultado=calcularFactorial(factorial);
-        String mensaje=generarMensaje(factorial, resultado);
+        Scanner entrada = new Scanner(System.in);
+        int factorial = newFactorial(entrada);
+        long resultado = calcularFactorial(factorial);
+        String mensaje = generarMensaje(factorial, resultado);
         mostrarMensaje(mensaje);
         entrada.close();
     }
